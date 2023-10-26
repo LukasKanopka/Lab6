a2ea97d3083b97e92fb1a56df8c79790925f3eb5
LukasKanopka <115489378+LukasKanopka@users.noreply.github.com>
2023-10-26 13:17:07 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+
+def encode(input_str):
+    new = []
+    for char in input_str:
+        char_value = int(char)
+        if char_value < 7:
+            new_char = str(char_value + 3)
+        else:
+            new_char = str(char_value - 7)
+        new.append(new_char)
+    return "".join(new)
+
+def decode(encoded_str):
+    original = []
+    for char in encoded_str:
+        char_value = int(char)
+        if char_value < 3:
+            original_char = str(char_value + 7)
+        else:
+            original_char = str(char_value - 3)
+        original.append(original_char)
+    return "".join(original)
+
+def print_menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+
+if __name__ == '__main__':
+    input_str = input("Enter a string: ")
+    new_str = encode(input_str)
+    print(new_str)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
